39eba2d03b8c850c7fa4f346e20caf3e4650e884
VincenzoNucci <vincenzo.nucci@studenti.unicam.it>
2020-07-18 08:55:35 +0000

    updated mean squared error

--- cost_functions.py
+++ cost_functions.py
@@ -4,9 +4,11 @@ import numpy as np
 # nel backward mi deve restituire un vettore
 class Cost_MSE():
     def forward(self, y_pred, y_real):
-        return np.sum((y_pred - y_real)**2, axis=0) / 2 # la somma dell'errore di tutti i neuroni k di output
+        n = y_real.shape[0]
+        return (1/n) * np.sum((y_pred - y_real)**2)  # la somma dell'errore di tutti i neuroni k di output
     def backward(self, y_pred, y_real):
-        return y_pred - y_real
+        m = y_real.shape[0]
+        return (2/m) * np.sum(y_pred - y_real)
 
 class CrossEntropyLoss():
     def forward(self, y_pred, y_real):
