fec729ff21683735578bbf04601ca3890fa0a80f
VincenzoNucci <vincenzo.nucci@studenti.unicam.it>
2020-07-19 22:37:25 +0000

    added categorical cross entropy loss

--- cost_functions.py
+++ cost_functions.py
@@ -6,12 +6,25 @@ class Cost_MSE():
     def forward(self, y_pred, y_real):
         n = y_real.shape[0]
         return (1/n) * np.sum((y_pred - y_real)**2)  # la somma dell'errore di tutti i neuroni k di output
+        # uso axis=0 nel caso di categorical loss
     def backward(self, y_pred, y_real):
         m = y_real.shape[0]
-        return (2/m) * np.sum(y_pred - y_real)
+        return (2/m) * np.sum(y_pred - y_real, axis=0)
 
-class CrossEntropyLoss():
+# categorical cross entropy is when cross entropy is used together with softmax
+# binary cross entropy is when cross entropy is used together with sigmoid
+class Categorical_CrossEntropyLoss():
     def forward(self, y_pred, y_real):
+        predictions = np.copy(y_pred)
+        predictions = np.clip(predictions, 1e-12, 1 - 1e-12) # avoid zero values for log
+        n = y_real.shape[0]
         return - np.sum(y_real * np.log(y_pred))
     def backward(self, y_pred, y_real):
-        return 0
+        return y_real - y_pred
+        pass
+
+class Binary_CrossEntropyLoss():
+    def forward(self, y_pred, y_real):
+        return - (y_real * np.log(y_pred) + (1 - y_real) * np.log(1 - y_pred))
+    def backward(self, y_pred, y_real):
+        pass
