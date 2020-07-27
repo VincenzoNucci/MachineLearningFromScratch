925147aff00bb14ef90ab4d1525a5f020f64e389
Vincenzo Nucci <vincenzo.nucci@studenti.unicam.it>
2020-07-27 19:13:56 +0200

    correct se_est impl

--- linear_models.py
+++ linear_models.py
@@ -44,27 +44,24 @@ class LinearRegressor(LinearModel):
     def fit(self, X, y, epochs=200, step=0.01):
         self.n_samples = X.shape[0]
         self.n_features = X.shape[1]
-        self.inputs = X
-        # define matrix w with instances plus bias
-        # self.theta = np.hstack((np.ones((X.shape[0], 1)),X))
-        self.theta = np.random.normal(size=(self.n_features + 1, 1))
         _X = np.hstack((np.ones((X.shape[0], 1)), X))
+        self.inputs = _X
 
-        for epoch in range(epochs):
-            y_pred = _X @ self.theta
-            # compute error with mse
-            self.error = (1/self.n_samples) * np.sum((y_pred - y)**2)
-            # compute gradient of cost
-            grad = (2/self.n_samples) * (_X.T.dot(_X.dot(self.theta) - y))
-            # update weights
-            self.theta -= step * grad
-            print('epoch: {}, error: {}'.format(epoch+1, self.error))
-            
-            #errore = self.cost.forward(self.predict(X),y)
-            #print('epoch: {}, error: {}'.format(epoch+1, errore))
-            #error.append(errore)
-            #self.theta -= step * self.cost.backward(errore, y)
-        
+        # gradient descent approach
+        # self.theta = np.random.normal(size=(self.n_features + 1, 1))
+        # _X = np.hstack((np.ones((X.shape[0], 1)), X))
+
+        #for epoch in range(epochs):
+        #    y_pred = _X @ self.theta
+        #    # compute error with mse
+        #    self.error = (1/self.n_samples) * np.sum((y_pred - y)**2)
+        #    # compute gradient of cost
+        #    grad = (2/self.n_samples) * (_X.T.dot(_X.dot(self.theta) - y))
+        #    # update weights
+        #    self.theta -= step * grad
+        #    print('epoch: {}, error: {}'.format(epoch+1, self.error))
+
+        # pseudo-inverse approach
         self.weights = np.hstack((np.ones((X.shape[0], 1)),X))
         # compute in a single step, the best parameters that fit the data
         self.theta = np.linalg.inv(np.dot(self.weights.T,self.weights)).dot(self.weights.T).dot(y)
@@ -79,10 +76,11 @@ class LinearRegressor(LinearModel):
         self.sst = np.sum((y - np.mean(y)**2))
         s = np.sqrt(self.sse/(self.n_samples - 2))
         self.se_est = np.sqrt(1 / (self.n_samples-2) * np.sum((y_pred - y)**2,axis=0) / np.sum((self.inputs - np.mean(self.inputs))**2,axis=0))
+        
         self.se_est = np.reshape(self.se_est, (-1, 1))
-        print(self.se_est)
+        
         self.t_value = np.abs(np.divide(self.theta, self.se_est))
-        print(np.hstack((self.theta, self.se_est, self.t_value)))
+        
 
         
 
@@ -101,15 +99,12 @@ class LinearRegressor(LinearModel):
         return 1 - (1 - r2_biased) * ((X.shape[0] - 1)/(X.shape[0] - X.shape[1] - 1))
 
     def summary(self):
-        summary = np.zeros((self.n_features + 1, 4))
-        print(self.theta)
-        print(self.se_est)
-        print(self.t_value)
+        print(np.hstack((self.theta, self.se_est, self.t_value)))
         
 
     # Intepretation Methods
 
-    def weight_plot(self, coefs_, n_features, feature_names):
+    def weight_plot(self, feature_names):
         ci = np.zeros((2,self.n_features))
         plt.scatter(self.coef_, np.arange(0,self.n_features), c='k', s=20)
         plt.title('Weight plot')
@@ -120,10 +115,10 @@ class LinearRegressor(LinearModel):
         alpha = 0.05
         df = self.n_features
         t = stats.t.ppf(1 - alpha/2, df)
-        s = np.std(self.theta, ddof=1)
-        n = self.theta.shape[0]
-        ci[0,:] = np.mean(self.theta) - (t * s / np.sqrt(n))
-        ci[1,:] = np.mean(self.theta) + (t * s / np.sqrt(n))
+        s = np.std(self.coef_, ddof=1)
+        n = self.coef_.shape[0]
+        ci[0,:] = np.mean(self.coef_) - (t * s / np.sqrt(n))
+        ci[1,:] = np.mean(self.coef_) + (t * s / np.sqrt(n))
 
         # ms = marker size, quando è grande il pallino
         # ecolor = il colore della barra del conf int
@@ -138,16 +133,14 @@ class LinearRegressor(LinearModel):
 if __name__ == "__main__":
     # dataset = np.genfromtxt('./Bike-Sharing-Dataset/day.csv',delimiter=',',dtype=None, encoding=None, names=True)
     df = pd.read_csv('./Bike-Sharing-Dataset/day.csv', header=0, parse_dates=True)
-    # No uso categorical, lasciamo interi come sono
+    
     # qui fare preprocessing come su github
-    # season to categorical
-    df['seasonSUMMER'] = 0
-    df['seasonFALL'] = 0
-    df['seasonWINTER'] = 0
-
-    df.loc[df['season'] == 2, ['seasonSUMMER']] = 1
-    df.loc[df['season'] == 3, ['seasonFALL']] = 1
-    df.loc[df['season'] == 4, ['seasonWINTER']] = 1
+    df['season'] = df['season'].map({2:'seasonSUMMER', 3:'seasonFALL',4:'seasonWINTER'})
+    df['holiday'] = df['holiday'].map({1:'holidayHOLIDAY'})
+    df['workingday'] = df['workingday'].map({1:'workingdayWORKING DAY'})
+    df['weathersit'] = df['weathersit'].map({1:'weathersitGOOD',2:'weathersitMISTY',3:'weathersitRAIN/SNOW/STORM'})
+    df = pd.get_dummies(df, prefix='', prefix_sep='',columns=['season','holiday','workingday','weathersit'])
+    
     # weekday to categorical
     df.loc[df['weekday'] == 0, ['weekday']] = 'SUN'
     df.loc[df['weekday'] == 1, ['weekday']] = 'MON'
@@ -156,21 +149,7 @@ if __name__ == "__main__":
     df.loc[df['weekday'] == 4, ['weekday']] = 'THU'
     df.loc[df['weekday'] == 5, ['weekday']] = 'FRI'
     df.loc[df['weekday'] == 6, ['weekday']] = 'SAT'
-    # holiday to categorical
-    df['holidayHOLIDAY'] = df['holiday']
-    # df.loc[df['holiday'] == 0, ['holiday']] = 'NO HOLIDAY'
-    # df.loc[df['holiday'] == 1, ['holiday']] = 'HOLIDAY'
-    # workingday to categorical
-    df['workingdayWORKING DAY'] = df['workingday']
-    # df.loc[df['workingday'] == 0, ['workingday']] = 'NO WORKING DAY'
-    # df.loc[df['workingday'] == 1, ['workingdayWORKING DAY']] = 1
-    # weathersit to categorical
-    df['weathersitGOOD'] = 0
-    df['weathersitMISTY'] = 0
-    df['weathersitRAIN/SNOW/STORM'] = 0
-    df.loc[df['weathersit'] == 1, ['weathersitGOOD']] = 1
-    df.loc[df['weathersit'] == 2, ['weathersitMISTY']] = 1
-    df.loc[df['weathersit'] == 3, ['weathersitRAIN/SNOW/STORM']] = 1
+    
     # mnth to categorical
     df.loc[df['mnth'] == 1, ['mnth']] = 'JAN'
     df.loc[df['mnth'] == 2, ['mnth']] = 'FEB'
@@ -195,7 +174,7 @@ if __name__ == "__main__":
     df['windspeed'] = 67 * df['windspeed']
     df['hum'] = 100 * df['hum']
     
-    X = df.drop(labels=['instant','dteday','registered','casual','atemp',  'holiday','workingday','weathersit','mnth','yr','season','weekday'],axis=1)
+    X = df.drop(labels=['instant','dteday','registered','casual','atemp' ,'mnth','yr','weekday'],axis=1)
     X = X.drop(labels=['cnt'],axis=1)
     
     labels = list(X.columns)
@@ -223,7 +202,7 @@ if __name__ == "__main__":
 
     #y_pred = model.predict(X_test)
     
-    #model.weight_plot(labels)
+    myModel.weight_plot(labels)
 
 
     
