48b9dce96416c491c9e7a57ff257419fdad4e049
VincenzoNucci <vincenzo.nucci@studenti.unicam.it>
2020-07-29 19:13:45 +0200

    added effect plot

--- linear_models.py
+++ linear_models.py
@@ -5,9 +5,11 @@ import pandas as pd
 from sklearn.datasets import load_boston
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
 import matplotlib.pyplot as plt
 
-from preprocessing_functions import StandardScaler
+from preprocessing_functions import StandardScaler as scaler
 
 class LinearModel(object):
     def __init__(self):
@@ -44,8 +46,8 @@ class LinearRegressor(LinearModel):
     def fit(self, X, y, epochs=200, step=0.01):
         self.n_samples = X.shape[0]
         self.n_features = X.shape[1]
-        _X = np.hstack((np.ones((X.shape[0], 1)), X))
-        self.inputs = _X
+        # _X = np.hstack((np.ones((X.shape[0], 1)), X))
+        self.inputs = X
 
         # gradient descent approach
         # self.theta = np.random.normal(size=(self.n_features + 1, 1))
@@ -66,20 +68,25 @@ class LinearRegressor(LinearModel):
         # compute in a single step, the best parameters that fit the data
         self.theta = np.linalg.inv(np.dot(self.weights.T,self.weights)).dot(self.weights.T).dot(y)
         self.theta = np.reshape(self.theta, (-1,1)) # reshape into a 2d vector
+
+        # Q, R = np.linalg.qr(self.inputs)
+        # self.theta = np.linalg.inv(R).dot(Q.T).dot(y)
+
         self.intercept_ = self.theta[0]
         self.coef_ = self.theta[1:]
         # print('theta best', self.theta.shape) (14,1)
+        _X = np.hstack((np.ones((X.shape[0], 1)), X))
         y_pred = _X @ self.theta
         # compute error with mse
         self.error = (1/self.n_samples) * np.sum((y_pred - y)**2)
         self.sse = np.sum((y - y_pred)**2)
-        self.sst = np.sum((y - np.mean(y)**2))
+        #self.sst = np.sum((y - np.mean(y))**2)
         s = np.sqrt(self.sse/(self.n_samples - 2))
         self.se_est = np.sqrt(1 / (self.n_samples-2) * np.sum((y_pred - y)**2,axis=0) / np.sum((self.inputs - np.mean(self.inputs))**2,axis=0))
         
         self.se_est = np.reshape(self.se_est, (-1, 1))
         
-        self.t_value = np.abs(np.divide(self.theta, self.se_est))
+        # self.t_value = np.abs(np.divide(self.theta, self.se_est))
         
 
         
@@ -94,9 +101,11 @@ class LinearRegressor(LinearModel):
     def score(self, X, y):
         y_pred = self.predict(X)
         sse = np.sum((y - y_pred)**2)
-        sst = np.sum((y - np.mean(y)**2))
+        sst = np.sum((y - np.mean(y))**2)
         r2_biased = 1 - sse / sst
-        return 1 - (1 - r2_biased) * ((X.shape[0] - 1)/(X.shape[0] - X.shape[1] - 1))
+        return r2_biased
+        # adjusted r squared
+        #return 1 - (1 - r2_biased) * ((X.shape[0] - 1)/(X.shape[0] - X.shape[1] - 1))
 
     def summary(self):
         print(np.hstack((self.theta, self.se_est, self.t_value)))
@@ -106,7 +115,7 @@ class LinearRegressor(LinearModel):
 
     def weight_plot(self, feature_names):
         ci = np.zeros((2,self.n_features))
-        plt.scatter(self.coef_, np.arange(0,self.n_features), c='k', s=20)
+        #plt.scatter(self.coef_, np.arange(0,self.n_features), c='k', s=20)
         plt.title('Weight plot')
         plt.xlabel('Weight estimate')
         plt.yticks(ticks=np.arange(0,self.n_features),labels=feature_names)
@@ -117,17 +126,32 @@ class LinearRegressor(LinearModel):
         t = stats.t.ppf(1 - alpha/2, df)
         s = np.std(self.coef_, ddof=1)
         n = self.coef_.shape[0]
-        ci[0,:] = np.mean(self.coef_) - (t * s / np.sqrt(n))
-        ci[1,:] = np.mean(self.coef_) + (t * s / np.sqrt(n))
-
+        #ci[0,:] = self.coef_.flatten() - (t * s / np.sqrt(n))
+        #ci[1,:] = self.coef_.flatten() + (t * s / np.sqrt(n))
+        lower, upper = stats.norm.interval(0.05,loc=self.coef_, scale=s/np.sqrt(n))
+        ci[0,:] = lower.flatten()
+        ci[1,:] = upper.flatten()
         # ms = marker size, quando è grande il pallino
         # ecolor = il colore della barra del conf int
         # elinewidth = quanto è thicc la barra del conf int
         # capsize = quanto grandi le barre laterali che chiudono il conf int
         # fmt = 'o' significa disegna solo la pallina
         # xerr = tupla con il lower e upper, specificando solo xerr, il conf int viene orizzontale
-        plt.errorbar(self.coef_, np.arange(self.n_features), xerr=ci, fmt='o', ms=0, c='k', ecolor='k', elinewidth=1.5, capsize=2.5)
+        plt.errorbar(self.coef_, np.arange(self.n_features), xerr=ci, fmt='o', ms=5, c='k', ecolor='k', elinewidth=1.5, capsize=2.5)
         plt.axvline(0, linestyle=':', c='k')
+        plt.show()
+
+    def effect_plot(self, feature_names):
+        # calculate the effect for the data
+        # self.effects = np.zeros_like(self.inputs)
+        self.effects = np.multiply(self.inputs, self.coef_.T)
+        #for i,input in enumerate(self.inputs):
+        #    self.effects[i,:] = np.multiply(input, self.coef_.T)
+        plt.title('Effect plot')
+        plt.xlabel('Feature effect')
+        plt.yticks(ticks=np.arange(0,self.n_features),labels=feature_names)
+        plt.axvline(0, linestyle=':', c='k')
+        plt.boxplot(self.effects, vert=False, labels=feature_names)
         plt.show()
 
 if __name__ == "__main__":
@@ -179,12 +203,13 @@ if __name__ == "__main__":
     
     labels = list(X.columns)
     X = X.to_numpy()
-    X = StandardScaler().fit(X)
+    X = StandardScaler().fit_transform(X)
     y = np.reshape(df['cnt'].to_numpy(), (-1,1))
 
     #dataset = load_boston()
+    #print(dataset.DESCR)
     #X = dataset.data
-    #X = StandardScaler().fit(X)
+    #X = StandardScaler().fit_transform(X)
     #y = np.reshape(dataset.target, (-1,1))
     #labels = dataset.feature_names
     
@@ -192,17 +217,18 @@ if __name__ == "__main__":
 
     myModel = LinearRegressor().fit(X_train, y_train, epochs=1)
     model = LinearRegression().fit(X_train,y_train)
-    #print(model.score(X_test, y_test))
+    # print(model.score(X_test, y_test))
     print(myModel.score(X_test, y_test))
-    #print(model.coef_.T - myModel.coef_)
+    # print(model.coef_.T - myModel.coef_)
     # print(myModel.coef_)
     #model.fit(X_train, y_train, epochs=10, step=0.02)
-    myModel.summary()
+    # myModel.summary()
     # one = np.reshape(X_test[0], (1,-1))
 
     #y_pred = model.predict(X_test)
     
     myModel.weight_plot(labels)
+    myModel.effect_plot(labels)
 
 
     
