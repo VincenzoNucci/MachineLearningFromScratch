276e34b1080345d54f9fb91f77f79bf94dd8b1d0
VincenzoNucci <vincenzo.nucci@studenti.unicam.it>
2020-08-03 06:25:58 +0200

    logistic regression

--- linear_models.py
+++ linear_models.py
@@ -1,14 +1,16 @@
 import numpy as np
 from datetime import datetime
 from scipy import stats
+from scipy.special import expit # stable logistic function
 import pandas as pd
-from sklearn.datasets import load_boston
+from sklearn.datasets import load_boston, load_iris
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
-from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
 from sklearn.decomposition import PCA
 import matplotlib.pyplot as plt
-
+import eli5
+from IPython.display import display
 from preprocessing_functions import StandardScaler as scaler
 
 class LinearModel(object):
@@ -38,80 +40,7 @@ class LinearModel(object):
         return [np.abs(self.theta[i] / self.se()[i]) for i in range(self.n_features)]
         # return np.abs(self.theta / self.se())
 
-class LinearRegressor(LinearModel):
-    def __init__(self):
-        super().__init__()
-        # self.weights è matrice Nxd+1 (compreso il bias)
-
-    def fit(self, X, y, epochs=200, step=0.01):
-        self.n_samples = X.shape[0]
-        self.n_features = X.shape[1]
-        # _X = np.hstack((np.ones((X.shape[0], 1)), X))
-        self.inputs = X
-
-        # gradient descent approach
-        # self.theta = np.random.normal(size=(self.n_features + 1, 1))
-        # _X = np.hstack((np.ones((X.shape[0], 1)), X))
-
-        #for epoch in range(epochs):
-        #    y_pred = _X @ self.theta
-        #    # compute error with mse
-        #    self.error = (1/self.n_samples) * np.sum((y_pred - y)**2)
-        #    # compute gradient of cost
-        #    grad = (2/self.n_samples) * (_X.T.dot(_X.dot(self.theta) - y))
-        #    # update weights
-        #    self.theta -= step * grad
-        #    print('epoch: {}, error: {}'.format(epoch+1, self.error))
-
-        # pseudo-inverse approach
-        self.weights = np.hstack((np.ones((X.shape[0], 1)),X))
-        # compute in a single step, the best parameters that fit the data
-        self.theta = np.linalg.inv(np.dot(self.weights.T,self.weights)).dot(self.weights.T).dot(y)
-        self.theta = np.reshape(self.theta, (-1,1)) # reshape into a 2d vector
-
-        # Q, R = np.linalg.qr(self.inputs)
-        # self.theta = np.linalg.inv(R).dot(Q.T).dot(y)
-
-        self.intercept_ = self.theta[0]
-        self.coef_ = self.theta[1:]
-        # print('theta best', self.theta.shape) (14,1)
-        _X = np.hstack((np.ones((X.shape[0], 1)), X))
-        y_pred = _X @ self.theta
-        # compute error with mse
-        self.error = (1/self.n_samples) * np.sum((y_pred - y)**2)
-        self.sse = np.sum((y - y_pred)**2)
-        #self.sst = np.sum((y - np.mean(y))**2)
-        s = np.sqrt(self.sse/(self.n_samples - 2))
-        self.se_est = np.sqrt(1 / (self.n_samples-2) * np.sum((y_pred - y)**2,axis=0) / np.sum((self.inputs - np.mean(self.inputs))**2,axis=0))
-        
-        self.se_est = np.reshape(self.se_est, (-1, 1))
-        
-        # self.t_value = np.abs(np.divide(self.theta, self.se_est))
-        
-
-        
-
-        return self
-
-    def predict(self, X):
-        # if passed only a sample instead of test matrix
-        _X = np.hstack((np.ones((X.shape[0], 1)), X))
-        return _X @ self.theta
-
-    def score(self, X, y):
-        y_pred = self.predict(X)
-        sse = np.sum((y - y_pred)**2)
-        sst = np.sum((y - np.mean(y))**2)
-        r2_biased = 1 - sse / sst
-        return r2_biased
-        # adjusted r squared
-        #return 1 - (1 - r2_biased) * ((X.shape[0] - 1)/(X.shape[0] - X.shape[1] - 1))
-
-    def summary(self):
-        print(np.hstack((self.theta, self.se_est, self.t_value)))
-        
-
-    # Intepretation Methods
+        # Intepretation Methods
 
     def weight_plot(self, feature_names):
         ci = np.zeros((2,self.n_features))
@@ -128,7 +57,7 @@ class LinearRegressor(LinearModel):
         n = self.coef_.shape[0]
         #ci[0,:] = self.coef_.flatten() - (t * s / np.sqrt(n))
         #ci[1,:] = self.coef_.flatten() + (t * s / np.sqrt(n))
-        lower, upper = stats.norm.interval(0.05,loc=self.coef_, scale=s/np.sqrt(n))
+        lower, upper = stats.norm.interval(alpha,loc=self.coef_, scale=s/np.sqrt(n))
         ci[0,:] = lower.flatten()
         ci[1,:] = upper.flatten()
         # ms = marker size, quando è grande il pallino
@@ -154,7 +83,117 @@ class LinearRegressor(LinearModel):
         plt.boxplot(self.effects, vert=False, labels=feature_names)
         plt.show()
 
+class LinearRegressor(LinearModel):
+    def __init__(self):
+        super().__init__()
+        # self.weights è matrice Nxd+1 (compreso il bias)
+
+    def fit(self, X, y, epochs=200, step=0.01):
+        self.n_samples = X.shape[0]
+        self.n_features = X.shape[1]
+        # _X = np.hstack((np.ones((X.shape[0], 1)), X))
+        self.inputs = X
+
+        # gradient descent approach
+        # self.theta = np.random.normal(size=(self.n_features + 1, 1))
+        # _X = np.hstack((np.ones((X.shape[0], 1)), X))
+
+        #for epoch in range(epochs):
+        #    y_pred = _X @ self.theta
+        #    # compute error with mse
+        #    self.error = (1/self.n_samples) * np.sum((y_pred - y)**2)
+        #    # compute gradient of cost
+        #    grad = (2/self.n_samples) * (_X.T.dot(_X.dot(self.theta) - y))
+        #    # update weights
+        #    self.theta -= step * grad
+        #    print('epoch: {}, error: {}'.format(epoch+1, self.error))
+
+        # Moore-Penrose pseudo-inverse approach
+        self.weights = np.hstack((np.ones((X.shape[0], 1)),X))
+        # compute in a single step, the best parameters that fit the data
+        self.theta = np.linalg.inv(np.dot(self.weights.T,self.weights)).dot(self.weights.T).dot(y)
+        self.theta = np.reshape(self.theta, (-1,1)) # reshape into a 2d vector
+
+        # QR decomposition approach
+        # _X = np.hstack((np.ones((X.shape[0], 1)), X))
+        # Q, R = np.linalg.qr(_X)
+        # self.theta = np.linalg.inv(R).dot(Q.T).dot(y)
+
+        # SVD approach
+        # TODO
+
+        self.intercept_ = self.theta[0]
+        self.coef_ = self.theta[1:]
+        # print('theta best', self.theta.shape) (14,1)
+        _X = np.hstack((np.ones((X.shape[0], 1)), X))
+        y_pred = _X @ self.theta
+        # compute error with mse
+        self.error = (1/self.n_samples) * np.sum((y_pred - y)**2)
+        self.sse = np.sum((y - y_pred)**2)
+        #self.sst = np.sum((y - np.mean(y))**2)
+        s = np.sqrt(self.sse/(self.n_samples - 2))
+        #self.se_est = np.sqrt(s) / np.sqrt(np.diag(np.linalg.inv(np.dot(self.weights.T,self.weights))))
+        
+        #self.se_est = np.reshape(self.se_est, (-1, 1))
+        
+        #self.t_value = np.abs(np.divide(self.theta, self.se_est))
+        
+
+        
+
+        return self
+
+    def predict(self, X):
+        # if passed only a sample instead of test matrix
+        _X = np.hstack((np.ones((X.shape[0], 1)), X))
+        return _X @ self.theta
+
+    def score(self, X, y):
+        y_pred = self.predict(X)
+        sse = np.sum((y - y_pred)**2)
+        sst = np.sum((y - np.mean(y))**2)
+        r2_biased = 1 - sse / sst
+        return r2_biased
+        # adjusted r squared
+        #return 1 - (1 - r2_biased) * ((X.shape[0] - 1)/(X.shape[0] - X.shape[1] - 1))
+
+    def summary(self, feature_names):
+        print(np.hstack((self.theta, self.se_est, self.t_value)))
+        
+
+
+
+class LogisticRegressor(LinearModel):
+    def __init__(self):
+        super().__init__()
+
+    def predict(self, X):
+        _X = np.hstack((np.ones((X.shape[0], 1)), X))
+        return expit(_X @ self.theta)
+
+    def fit(self, X, y, epochs=200, step=0.01):
+        self.n_samples = X.shape[0]
+        self.n_features = X.shape[1]
+        self.inputs = X
+        self.theta = np.random.normal(loc=0.1, scale=2, size=(self.n_features + 1,1))
+        self.theta[0,:] = 1 # initialize bias (w_0)
+        self.error = 0
+        for epoch in range(epochs):
+            y_pred = self.predict(self.inputs)
+            print(y_pred)
+            error = - np.sum(np.sum(y * np.log(y_pred),axis=1),axis=0) # multiclass cross-entropy error
+            # error = 1/self.n_samples * - np.sum(y * np.log(y_pred)) # cross entropy error for multi-class > 2
+            self.error = error
+            grad = np.sum((y_pred - y)* self.theta) 
+            self.theta -= step * grad
+            print('epoch: {}, error: {}'.format(epoch+1, error))
+        
+        self.intercept_ = self.theta[0]
+        self.coef_ = self.theta[1:]
+        return self
+
 if __name__ == "__main__":
+    np.random.seed(1)
     # dataset = np.genfromtxt('./Bike-Sharing-Dataset/day.csv',delimiter=',',dtype=None, encoding=None, names=True)
     df = pd.read_csv('./Bike-Sharing-Dataset/day.csv', header=0, parse_dates=True)
     
@@ -216,13 +255,14 @@ if __name__ == "__main__":
     X_train, X_test, y_train, y_test = train_test_split(X,y,train_size=0.8, random_state=0)
 
     myModel = LinearRegressor().fit(X_train, y_train, epochs=1)
+    display(eli5.show_weights(myModel))
     model = LinearRegression().fit(X_train,y_train)
     # print(model.score(X_test, y_test))
     print(myModel.score(X_test, y_test))
     # print(model.coef_.T - myModel.coef_)
     # print(myModel.coef_)
     #model.fit(X_train, y_train, epochs=10, step=0.02)
-    # myModel.summary()
+    #myModel.summary(labels)
     # one = np.reshape(X_test[0], (1,-1))
 
     #y_pred = model.predict(X_test)
@@ -230,5 +270,14 @@ if __name__ == "__main__":
     myModel.weight_plot(labels)
     myModel.effect_plot(labels)
 
+    # Logistic Regression test
 
-    
+    dataset = load_iris()
+    X = dataset.data
+    X = StandardScaler().fit_transform(X)
+    y = np.reshape(dataset.target,(-1,1))
+    y = OneHotEncoder(sparse=False).fit_transform(y)
+    labels = dataset.feature_names
+
+    myLogRegressor = LogisticRegressor()
+    myLogRegressor.fit(X, y, epochs=1000, step=1e-3)
