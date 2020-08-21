e01e4a5523ca8a038481c28026b8513f22cad18c
VincenzoNucci <vincenzo.nucci@studenti.unicam.it>
2020-08-21 09:39:39 +0000

    weight plot added intercept

--- model_interpretable_methods.py
+++ model_interpretable_methods.py
@@ -16,8 +16,8 @@ def effect_plot():
     plt.boxplot(self.effects, vert=False, labels=feature_names)
     plt.show()
 
-def weight_plot(coef_y,y_pred,k,labels):
-    ci = np.zeros((2,k))
+def weight_plot(coef_,y,y_pred,k,labels):
+    ci = np.zeros((2,k+1))
     #plt.scatter(self.coef_, np.arange(0,self.n_features), c='k', s=20)
     plt.title('Weight plot')
     plt.xlabel('Weight estimate')
@@ -25,21 +25,22 @@ def weight_plot(coef_y,y_pred,k,labels):
     
     # Compute CI
     alpha = 0.05
-    df = k - 1
-    t = stats.norm.ppf(alpha/2, df)
-    s = np.std(coef_, ddof=1)
-    n = coef_.shape[0]
+    df = k
+    #t = stats.norm.ppf(alpha/2, df)
+    #s = np.std(coef_, ddof=1)
+    #n = coef_.shape[0]
     #ci[0,:] = self.coef_.flatten() - (t * s / np.sqrt(n))
     #ci[1,:] = self.coef_.flatten() + (t * s / np.sqrt(n))
     # lower, upper = stats.norm.interval(alpha,loc=self.coef_, scale=s/np.sqrt(n))
-    ci[0,:] = coef_ - stats.norm.ppf(alpha/2,df) * standard_error(y,y_pred,k,coef_)
-    ci[1,:] = coef_ + stats.norm.ppf(alpha/2,df) * standard_error(y,y_pred,k,coef_)
+    ci[0,:] = (coef_ - stats.norm.ppf(alpha/2,df) * standard_error(y,y_pred,k,coef_)).flatten()
+    ci[1,:] = (coef_ + stats.norm.ppf(alpha/2,df) * standard_error(y,y_pred,k,coef_)).flatten()
+
     # ms = marker size, quando è grande il pallino
     # ecolor = il colore della barra del conf int
     # elinewidth = quanto è thicc la barra del conf int
     # capsize = quanto grandi le barre laterali che chiudono il conf int
     # fmt = 'o' significa disegna solo la pallina
     # xerr = tupla con il lower e upper. specificando solo xerr, il conf int viene orizzontale
-    plt.errorbar(coef_, np.arange(k), xerr=ci, fmt='o', ms=5, c='k', ecolor='k', elinewidth=1.5, capsize=2.5)
+    plt.errorbar(coef_, np.arange(k+1), xerr=ci, fmt='o', ms=5, c='k', ecolor='k', elinewidth=1.5, capsize=2.5)
     plt.axvline(0, linestyle=':', c='k')
     plt.show()
