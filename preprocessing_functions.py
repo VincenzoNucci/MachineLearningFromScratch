bea76ab7f1f2f692b65dc9aac3ece3b36e4464cd
VincenzoNucci <vincenzo.nucci@studenti.unicam.it>
2020-12-07 18:15:22 +0100

    removed

--- preprocessing_functions.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-class StandardScaler:
-    def fit(self, inputs):
-        return (inputs - np.mean(inputs)) / np.std(inputs)
-
-class Normalizer:
-    def fit(self, inputs):
-        return (inputs - np.min(inputs)) / (np.max(inputs) - np.min(inputs))
